c91d30750c1d88c19cf4447096a873c01c402d83
xiaol <xiaoleliang045@gmail.com>
2021-03-19 17:11:13 +0900

    王老师学习内容

--- springBatch/src/main/java/com/koiwaLearning/api/mapper/StudentsMapper.java
+++ springBatch/src/main/java/com/koiwaLearning/api/mapper/StudentsMapper.java
@@ -7,5 +7,15 @@ import java.util.List;
 
 @Repository
 public interface StudentsMapper {
+    // 查询
     List<StudentsInfo> getStudentsInfo();
+
+    // 追加
+    int insertStudents(StudentsInfo students);
+
+    // 更新
+    int updateStudents(StudentsInfo students);
+
+    // 削除
+    int deleteStudents(String sno);
 }
--- springBatch/src/main/java/com/koiwaLearning/api/service/StudentsService.java
+++ springBatch/src/main/java/com/koiwaLearning/api/service/StudentsService.java
@@ -16,4 +16,18 @@ public class StudentsService {
     public List<StudentsInfo> getStudentsInfo() {
         return studentsMapper.getStudentsInfo();
     }
+
+    // 追加
+    public int insertStudents(StudentsInfo students) {
+        return studentsMapper.insertStudents(students);
+    }
+
+    // 更新
+    public int updateStudents(StudentsInfo students) {
+        return studentsMapper.updateStudents(students);
+    }
+    // 削除
+    public int deleteStudents(String sno) {
+        return studentsMapper.deleteStudents(sno);
+    }
 }
